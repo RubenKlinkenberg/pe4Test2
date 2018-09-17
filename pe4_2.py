f38aaabe1d0fb7cdb784225e991b156eba3ec76a
ruben <ruben.klinkenberg@student.hu.nl>
2018-09-17 11:20:09 +0200

    Itsme

---
+++ pe4_2.py
@@ -0,0 +1,5 @@
+urenloon = float(input('Wat verdien je per uur: '))
+werkuren = int(input('Hoeveel uur heb je gewerkt: '))
+totaalgeld = urenloon * werkuren
+line = str (werkuren) + ' uur werken levert je ' + str (totaalgeld) + ' Euro op.'
+print (line)
